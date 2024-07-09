be8c3f6a11c3f4af789ff8ba51a8a9d460568a9f
Bích Ngọc <132219708+ngocphungg@users.noreply.github.com>
2024-07-09 21:44:56 +0700

    Update AccSave.php

--- AccSave.php
+++ AccSave.php
@@ -11,7 +11,7 @@
 	$email=$_REQUEST["txt_email"];
 	$pass=$_REQUEST["passw"];
 	
-	$conn=mysqli_connect("localhost","root","") or die("disconnect");
+	$conn=mysqli_connect("localhost","ngoc","@Ngoc1206") or die("disconnect");
 	mysqli_select_db($conn,"qlchitieu")or die("not found");
 	$sql_savehsx="INSERT INTO `user` (`id_tk`,`email`,`name`,`pass`) VALUES (NULL, '$email','$name','$pass')";
 	mysqli_query($conn, $sql_savehsx);
